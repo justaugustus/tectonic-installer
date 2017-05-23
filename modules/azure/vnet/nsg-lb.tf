ac59066c41f41e28072f8f991411c0a7ad020daf
Stephen Augustus <augustus@coreos.com>
2017-05-23 14:47:48 -0700

    azure: Update nsg-lb templates

--- modules/azure/vnet/nsg-lb.tf
+++ modules/azure/vnet/nsg-lb.tf
@@ -1,11 +1,11 @@
-resource "azurerm_network_security_group" "lb" {
-  name                = "tectonic-cluster-${var.tectonic_cluster_name}-lb-nsg"
+resource "azurerm_network_security_group" "api" {
+  name                = "tectonic-cluster-${var.tectonic_cluster_name}-api-nsg"
   location            = "${var.tectonic_azure_location}"
   resource_group_name = "tectonic-cluster-${var.tectonic_cluster_name}"
 }
 
-resource "azurerm_network_security_rule" "lb_egress" {
-  name                        = "${var.tectonic_cluster_name}-lb_egress"
+resource "azurerm_network_security_rule" "api_egress" {
+  name                        = "${var.tectonic_cluster_name}-api_egress"
   priority                    = 100
   direction                   = "Outbound"
   access                      = "Allow"
@@ -15,33 +15,67 @@ resource "azurerm_network_security_rule" "lb_egress" {
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
   resource_group_name         = "tectonic-cluster-${var.tectonic_cluster_name}"
-  network_security_group_name = "tectonic-cluster-${var.tectonic_cluster_name}-lb-nsg"
+  network_security_group_name = "tectonic-cluster-${var.tectonic_cluster_name}-api-nsg"
 }
 
-resource "azurerm_network_security_rule" "lb_ingress_icmp" {
-  name                        = "${var.tectonic_cluster_name}-lb_ingress_icmp"
+resource "azurerm_network_security_rule" "api_ingress_https" {
+  name                        = "${var.tectonic_cluster_name}-api_ingress_https"
   priority                    = 200
   direction                   = "Inbound"
   access                      = "Allow"
-  protocol                    = "icmp"
+  protocol                    = "tcp"
+  source_port_range           = "443"
+  destination_port_range      = "443"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = "tectonic-cluster-${var.tectonic_cluster_name}"
+  network_security_group_name = "tectonic-cluster-${var.tectonic_cluster_name}-api-nsg"
+}
+
+resource "azurerm_network_security_group" "console" {
+  name                = "tectonic-cluster-${var.tectonic_cluster_name}-console-nsg"
+  location            = "${var.tectonic_azure_location}"
+  resource_group_name = "tectonic-cluster-${var.tectonic_cluster_name}"
+}
+
+resource "azurerm_network_security_rule" "console_egress" {
+  name                        = "${var.tectonic_cluster_name}-console_egress"
+  priority                    = 100
+  direction                   = "Outbound"
+  access                      = "Allow"
+  protocol                    = "*"
   source_port_range           = "*"
   destination_port_range      = "*"
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
   resource_group_name         = "tectonic-cluster-${var.tectonic_cluster_name}"
-  network_security_group_name = "tectonic-cluster-${var.tectonic_cluster_name}-lb-nsg"
+  network_security_group_name = "tectonic-cluster-${var.tectonic_cluster_name}-console-nsg"
 }
 
-resource "azurerm_network_security_rule" "lb_ingress_ssh" {
-  name                        = "${var.tectonic_cluster_name}-lb_ingress_ssh"
+resource "azurerm_network_security_rule" "console_ingress_https" {
+  name                        = "${var.tectonic_cluster_name}-console_ingress_https"
+  priority                    = 200
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "tcp"
+  source_port_range           = "443"
+  destination_port_range      = "443"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = "tectonic-cluster-${var.tectonic_cluster_name}"
+  network_security_group_name = "tectonic-cluster-${var.tectonic_cluster_name}-console-nsg"
+}
+
+resource "azurerm_network_security_rule" "console_ingress_http" {
+  name                        = "${var.tectonic_cluster_name}-console_ingress_http"
   priority                    = 300
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "tcp"
-  source_port_range           = "22"
-  destination_port_range      = "22"
+  source_port_range           = "80"
+  destination_port_range      = "80"
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
   resource_group_name         = "tectonic-cluster-${var.tectonic_cluster_name}"
-  network_security_group_name = "tectonic-cluster-${var.tectonic_cluster_name}-lb-nsg"
+  network_security_group_name = "tectonic-cluster-${var.tectonic_cluster_name}-console-nsg"
 }
