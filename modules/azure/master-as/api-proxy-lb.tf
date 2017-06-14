6e515f0f97f7f17e3a21114a8e278036addc24b1
Mike Metral <metral@gmail.com>
2017-06-14 19:52:05 +0000

    azure: vnet refactor & Nginx fixes

--- modules/azure/master-as/api-proxy-lb.tf
+++ modules/azure/master-as/api-proxy-lb.tf
@@ -2,38 +2,38 @@
 ## Related to: https://github.com/Microsoft/azure-docs/blob/master/articles/load-balancer/load-balancer-internal-overview.md#limitations
 
 resource "azurerm_lb" "proxy_lb" {
-  name                = "${var.cluster_name}-api-proxy-lb"
+  name                = "${var.cluster_name}-console-proxy-lb"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group_name}"
 
   frontend_ip_configuration {
-    name                          = "api-proxy"
+    name                          = "console-proxy"
     subnet_id                     = "${var.subnet}"
     private_ip_address_allocation = "dynamic"
   }
 }
 
-resource "azurerm_lb_backend_address_pool" "api-proxy-lb" {
+resource "azurerm_lb_backend_address_pool" "console-proxy-lb" {
   resource_group_name = "${var.resource_group_name}"
   loadbalancer_id     = "${azurerm_lb.proxy_lb.id}"
-  name                = "api-proxy-lb-pool"
+  name                = "console-proxy-lb-pool"
 }
 
-resource "azurerm_lb_rule" "api-proxy-lb" {
-  name                    = "api-proxy-lb-rule-443-443"
+resource "azurerm_lb_rule" "console-proxy-lb" {
+  name                    = "console-proxy-lb-rule-443-443"
   resource_group_name     = "${var.resource_group_name}"
   loadbalancer_id         = "${azurerm_lb.proxy_lb.id}"
-  backend_address_pool_id = "${azurerm_lb_backend_address_pool.api-proxy-lb.id}"
-  probe_id                = "${azurerm_lb_probe.api-proxy-lb.id}"
+  backend_address_pool_id = "${azurerm_lb_backend_address_pool.console-proxy-lb.id}"
+  probe_id                = "${azurerm_lb_probe.console-proxy-lb.id}"
 
   protocol                       = "tcp"
   frontend_port                  = 443
   backend_port                   = 443
-  frontend_ip_configuration_name = "api-proxy"
+  frontend_ip_configuration_name = "console-proxy"
 }
 
-resource "azurerm_lb_probe" "api-proxy-lb" {
-  name                = "api-proxy-lb-probe-443-up"
+resource "azurerm_lb_probe" "console-proxy-lb" {
+  name                = "console-proxy-lb-probe-443-up"
   loadbalancer_id     = "${azurerm_lb.proxy_lb.id}"
   resource_group_name = "${var.resource_group_name}"
   protocol            = "tcp"
