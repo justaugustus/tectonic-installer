d7c3b66bda90e815bb8493b23a0762010493bbb4
Stephen Augustus <augustus@coreos.com>
2017-07-16 01:00:26 -0700

    azure: Initial CDX rebase

--- modules/azure/worker-as/workers.tf
+++ modules/azure/worker-as/workers.tf
@@ -12,7 +12,7 @@ resource "azurerm_availability_set" "tectonic_workers" {
 
 resource "azurerm_virtual_machine" "tectonic_worker" {
   count                 = "${var.worker_count}"
-  name                  = "${format("%s-%s-%03d", var.cluster_name, var.role, count.index + 1)}"
+  name                  = "${format("%s%s%03d", var.cluster_name, "w", count.index + 1)}"
   location              = "${var.location}"
   resource_group_name   = "${var.resource_group_name}"
   network_interface_ids = ["${var.network_interface_ids[count.index]}"]
@@ -30,6 +30,7 @@ resource "azurerm_virtual_machine" "tectonic_worker" {
     sku       = "${var.cl_channel}"
     version   = "${var.versions["container_linux"]}"
   }
+  
   storage_os_disk {
     name              = "worker-${count.index}-os-${var.storage_id}"
     managed_disk_type = "${var.storage_type}"
@@ -37,12 +38,14 @@ resource "azurerm_virtual_machine" "tectonic_worker" {
     caching           = "ReadWrite"
     os_type           = "linux"
   }
+
   os_profile {
     computer_name  = "${format("%s%s%03d", var.cluster_name, "w", count.index + 1)}"
     admin_username = "core"
     admin_password = ""
     custom_data    = "${base64encode("${data.ignition_config.worker.rendered}")}"
   }
+
   os_profile_linux_config {
     disable_password_authentication = true
 
@@ -51,10 +54,12 @@ resource "azurerm_virtual_machine" "tectonic_worker" {
       key_data = "${file(var.public_ssh_key)}"
     }
   }
+
   tags = "${merge(map(
     "Name", "${var.cluster_name}-worker-${count.index}",
     "tectonicClusterID", "${var.cluster_id}"),
     var.extra_tags)}"
+
   lifecycle {
     ignore_changes = ["storage_data_disk"]
   }
