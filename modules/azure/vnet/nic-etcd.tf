023bdfd7374c93f5227034684e96bed0dcca3d74
Stephen Augustus <augustus@coreos.com>
2017-07-16 14:42:05 -0700

    azure/vnet: Fix `etcd_nic` NSG ref

--- modules/azure/vnet/nic-etcd.tf
+++ modules/azure/vnet/nic-etcd.tf
@@ -3,7 +3,7 @@ resource "azurerm_network_interface" "etcd_nic" {
   name                      = "${var.cluster_name}-etcd-nic-${count.index}"
   location                  = "${var.location}"
   resource_group_name       = "${var.resource_group_name}"
-  network_security_group_id = "${var.external_nsg_etcd_id == "" ? azurerm_network_security_group.etcd.id : var.external_nsg_etcd_id}"
+  network_security_group_id = "${var.external_nsg_etcd_id == "" ? join(" ", azurerm_network_security_group.etcd.*.id) : var.external_nsg_etcd_id}"
 
   ip_configuration {
     name                                    = "tectonic_etcd_configuration"
