e71d4d818520d4ccf8274258e5cdcf6cdc1ebd06
Stephen Augustus <augustus@coreos.com>
2017-07-16 04:07:57 -0700

    azure/vnet: Remove redundant `etcd_nic` def & update filename

--- modules/azure/vnet/lb-etcd.tf
+++ modules/azure/vnet/lb-etcd.tf
@@ -1,17 +1,3 @@
-resource "azurerm_network_interface" "etcd_nic" {
-  count               = "${var.etcd_count}"
-  name                = "${var.tectonic_cluster_name}-etcd-nic-${count.index}"
-  location            = "${var.location}"
-  resource_group_name = "${var.resource_group_name}"
-
-  ip_configuration {
-    name                                    = "tectonic_etcd_configuration"
-    subnet_id                               = "${var.external_vnet_name == "" ?  join(" ", azurerm_subnet.master_subnet.*.id) : var.external_master_subnet_id }"
-    private_ip_address_allocation           = "dynamic"
-    load_balancer_backend_address_pools_ids = ["${azurerm_lb_backend_address_pool.etcd-lb.id}"]
-  }
-}
-
 resource "azurerm_lb" "tectonic_etcd_lb" {
   name                = "${var.tectonic_cluster_name}-etcd-lb"
   location            = "${var.location}"
