a2261df4bc986725aba60ca52272f694b3296e17
Stephen Augustus <augustus@coreos.com>
2017-07-17 18:21:04 -0700

    azure: Consistent VM names for cloud provider usage

--- modules/azure/worker-as/workers.tf
+++ modules/azure/worker-as/workers.tf
@@ -12,7 +12,7 @@ resource "azurerm_availability_set" "tectonic_workers" {
 
 resource "azurerm_virtual_machine" "tectonic_worker" {
   count                 = "${var.worker_count}"
-  name                  = "${format("%s-%s-%03d", var.cluster_name, var.role, count.index + 1)}"
+  name                  = "${format("%s%s%03d", var.cluster_name, "w", count.index + 1)}"
   location              = "${var.location}"
   resource_group_name   = "${var.resource_group_name}"
   network_interface_ids = ["${var.network_interface_ids[count.index]}"]
