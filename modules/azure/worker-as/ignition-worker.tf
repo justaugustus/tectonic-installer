6bd624708669e7d5834998d340c083f49119b01c
Stephen Augustus <augustus@coreos.com>
2017-08-07 13:38:25 -0700

    azure: Re-enable CLUO, mask off on update-engine, locksmithd

--- modules/azure/worker-as/ignition-worker.tf
+++ modules/azure/worker-as/ignition-worker.tf
@@ -8,7 +8,6 @@ data "ignition_config" "worker" {
 
   systemd = [
     "${data.ignition_systemd_unit.docker.id}",
-    "${data.ignition_systemd_unit.update-engine.id}",
     "${data.ignition_systemd_unit.locksmithd.id}",
     "${data.ignition_systemd_unit.kubelet-worker.id}",
     "${module.net_ignition.tx-off_id}",
@@ -29,11 +28,6 @@ data "ignition_systemd_unit" "docker" {
       content = "[Service]\nEnvironment=\"DOCKER_OPTS=--log-opt max-size=50m --log-opt max-file=3\"\n"
     },
   ]
-}
-
-data "ignition_systemd_unit" "update-engine" {
-  name = "update-engine.service"
-  mask = true
 }
 
 data "ignition_systemd_unit" "locksmithd" {
